a870823c7589f511dd1d20a13d8faba1780013f4
eapfgws-dotcom <edua.pastene@duocuc.cl>
2026-05-12 01:28:32 -0400

    FIX0002 Aplicacion de lombok

--- src/main/java/datosmedicos_service/model/Medicamento.java
+++ src/main/java/datosmedicos_service/model/Medicamento.java
@@ -1,71 +1,34 @@
 package datosmedicos_service.model;
 
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-// Entidad que representa medicamentos asociados a una ficha clinica
 @Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Medicamento {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    
-
     private String nombre;
+
     private String dosis;
+
     private String frecuencia;
 
-    @ManyToOne
-    @JoinColumn(name = "ficha_id")
-    @JsonIgnore
-    private FichaClinica ficha;
+    private String observaciones;
 
-    public Medicamento() {
-    }
+    private Long ficha;
 
-    public Medicamento(String nombre, String dosis, String frecuencia, FichaClinica ficha) {
-        this.nombre = nombre;
-        this.dosis = dosis;
-        this.frecuencia = frecuencia;
-        this.ficha = ficha;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public String getDosis() {
-        return dosis;
-    }
-
-    public String getFrecuencia() {
-        return frecuencia;
-    }
-
-    public FichaClinica getFicha() {
-        return ficha;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public void setDosis(String dosis) {
-        this.dosis = dosis;
-    }
-
-    public void setFrecuencia(String frecuencia) {
-        this.frecuencia = frecuencia;
-    }
-
-    public void setFicha(FichaClinica ficha) {
-        this.ficha = ficha;
-    }
+   
 }
